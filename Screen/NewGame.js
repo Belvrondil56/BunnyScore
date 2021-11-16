f9e22ce40f95288db0c8c12aa693f79e7e08412f
Belvrondil56 <alexandre.keruzore@gmail.com>
2021-11-16 18:27:08 +0100

    new partie add check player

--- Screen/NewGame.js
+++ Screen/NewGame.js
@@ -1,6 +1,8 @@
+import { useLinkBuilder } from "@react-navigation/native";
 import React, {useEffect, useState} from "react";
 import {View, Text, StyleSheet, ScrollView, ImageBackground, KeyboardAvoidingView, TouchableOpacity, KeyboardAvoidingViewBase, TextInput} from "react-native";
 import { supabase } from "../supabaseClient";
+import uuid from 'react-native-uuid';
 
 const NewGame = props => {
 
@@ -16,7 +18,7 @@ const NewGame = props => {
     const [player3Points, setPlayer3Points] = useState(null);
     const [player4Points, setPlayer4Points] = useState(null);
     const [nbrPlayer, setNbrPlayer] = useState(2);
-    const [toto, setData] = useState(null);
+    const [players, setPlayers] = useState([]);
     const [scores, setScores] = useState(false);
 
 
@@ -41,15 +43,24 @@ const NewGame = props => {
         setPlayer4Points(null);
     }
 
-    function checkPlayer(player){
-        console.log("player", player)
+    function checkPlayer(players){
+        players.forEach(player => {
+            if(supabase.from('profiles').select('username').eq('username', player) != null){
+                console.log('joueur inscrit : ', player)
+            }
+            else {
+                supabase.from("profile").insert([{
+                    id: uuid.v4(),
+                    username: player,
+                    invited: true
+                }])
+            }
+        })
     }
 
     async function getData() {
         
         const {data, error} = await supabase.from('profiles').select('username')
-
-        console.log("player :", data)
 
         if (data!=null){
             console.log("le pseudo existe")
@@ -60,14 +71,23 @@ const NewGame = props => {
         return data;
     }
 
-    function handleSaveGame(){
-        haveScore()
-        console.log(scores);
-        console.log("players 1:", player1, player1Points);
-        console.log("players 2:", player2, player2Points);
-        console.log("players 3", player3, player3Points);
-        console.log("players 4:", player4, player4Points);
-        console.log('nombre de joueurs', nbrPlayer);
+    useEffect (() => {
+
+    })
+
+    function checkPlayerList(){
+        setPlayers([...players, player1])
+        setPlayers(players => [...players, player2])
+        if(newPlayer3==true){
+            setPlayers(players => [...players, player3])
+        } 
+        if (newPlayer4==true){
+            setPlayers(players => [...players, player4])
+        }
+        
+    }
+
+    async function handleSaveGame(){
         /*const {game, error} = await supabase.from("game").insert([
             {
               nbr_players: nbrPlayer
@@ -79,14 +99,40 @@ const NewGame = props => {
             return;
           }*/
         //const {data, error2} = await supabase.from('profiles').select();
-        let data = getData();
-        data.then(res => console.log("data :", res[0].username));
+        
+        haveScore();
+        
+        checkPlayerList();
+            
+        
+        //checkPlayer()
+
+
+        // Créé un tableau avec la liste de joueurs dans la bdd et vérifie si ceux de la partie existent dedans
+        /*const playerList = [];
+        players.forEach(element => playerList.push(element.username))
+        console.log(playerList)
+        const verif = playerList.includes(player2)
+        console.log("verif :", verif)*/
+
+        //Ajouter un joueur invité
+        /*const {player, error} = await supabase.from('profiles').insert([
+            {
+                id: uuid.v4(),
+                username: player2,
+                invited: true
+            },
+        ]);
+        if (error){
+            console.log(error?.message);
+            return;
+        }*/
     }
 
     function haveScore() {
         if(nbrPlayer==2 && player1Points>0 && player2Points>0){
             setScores(true);
-        } else if (nbrPlayer==3 && player1Points>0 && player2Points>0 && player3Points>0){
+        } else if (nbrPlayer==3 && player1Points>0 && player2Points>0 && (player3Points>0 || player4Points>0)){
             setScores(true);
         } else if (nbrPlayer==4 && player1Points>0 && player2Points>0 && player3Points>0 && player4Points>0){
             setScores(true);
@@ -94,7 +140,7 @@ const NewGame = props => {
             setScores(false);
         }
     }
-
+    console.log(players);
     return(
         <ImageBackground source={require("../assets/lapinGold.jpg")} style={ styles.imgBackground } resizeMode='cover' imageStyle= 
             {{opacity:0.24}} blurRadius={1}>
@@ -104,13 +150,13 @@ const NewGame = props => {
                 <Text style={styles.titreApp}>Nouvelle Partie</Text>
                 <View style={styles.blocBody}>
                     <View style={styles.playerBloc}>
-                        <TextInput placeholder="Entrez un pseudo" style={styles.input} onChangeText={(player) => {checkPlayer(player); setPlayer1(player)}}></TextInput>
-                        <TextInput placeholder="Entrez le score" keyboardType = 'numeric' style={styles.input} onChangeText={(playerPoints) => {checkPlayer(playerPoints); setPlayer1Points(playerPoints)}}></TextInput>
+                        <TextInput placeholder="Entrez un pseudo" style={styles.input} onChangeText={(player) => {setPlayer1(player)}}></TextInput>
+                        <TextInput placeholder="Entrez le score" keyboardType = 'numeric' style={styles.input} onChangeText={(playerPoints) => {setPlayer1Points(playerPoints)}}></TextInput>
                     </View>
                     {newPlayer3 ?
                     <View style={styles.playerBloc}>
-                        <TextInput placeholder="Entrez un pseudo" style={styles.input} onChangeText={(player) => {checkPlayer(player); setPlayer3(player)}}></TextInput>
-                        <TextInput placeholder="Entrez le score" keyboardType = 'numeric' style={styles.input} onChangeText={(playerPoints) => {checkPlayer(playerPoints); setPlayer3Points(playerPoints)}}></TextInput>
+                        <TextInput placeholder="Entrez un pseudo" style={styles.input} onChangeText={(player) => {setPlayer3(player)}}></TextInput>
+                        <TextInput placeholder="Entrez le score" keyboardType = 'numeric' style={styles.input} onChangeText={(playerPoints) => {setPlayer3Points(playerPoints)}}></TextInput>
                         <TouchableOpacity onPress={() => handleRemoveNewPlayer3()}>
                             <Text style={styles.cross}>X</Text>
                         </TouchableOpacity>
@@ -121,13 +167,13 @@ const NewGame = props => {
                         </TouchableOpacity>
                     </View> }
                     <View style={styles.playerBloc}>
-                        <TextInput placeholder="Entrez un pseudo" style={styles.input} onChangeText={(player) => {checkPlayer(player); setPlayer2(player)}}></TextInput>
-                        <TextInput placeholder="Entrez le score" keyboardType = 'numeric' style={styles.input} onChangeText={(playerPoints) => {checkPlayer(playerPoints); setPlayer2Points(playerPoints)}}></TextInput>
+                        <TextInput placeholder="Entrez un pseudo" style={styles.input} onChangeText={(player) => {setPlayer2(player)}}></TextInput>
+                        <TextInput placeholder="Entrez le score" keyboardType = 'numeric' style={styles.input} onChangeText={(playerPoints) => {setPlayer2Points(playerPoints)}}></TextInput>
                     </View>
                     {newPlayer4 ?  
                     <View style={styles.playerBloc}>
-                        <TextInput placeholder="Entrez un pseudo" style={styles.input} onChangeText={(player) => {checkPlayer(player); setPlayer4(player)}}></TextInput>
-                        <TextInput placeholder="Entrez le score" keyboardType = 'numeric' style={styles.input} onChangeText={(playerPoints) => {checkPlayer(playerPoints); setPlayer4Points(playerPoints)}}></TextInput>
+                        <TextInput placeholder="Entrez un pseudo" style={styles.input} onChangeText={(player) => {setPlayer4(player)}}></TextInput>
+                        <TextInput placeholder="Entrez le score" keyboardType = 'numeric' style={styles.input} onChangeText={(playerPoints) => {setPlayer4Points(playerPoints)}}></TextInput>
                         <TouchableOpacity onPress={() => handleRemoveNewPlayer4()}>
                             <Text style={styles.cross}>X</Text>
                         </TouchableOpacity>
